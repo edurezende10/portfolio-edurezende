6dce1be6d1d3b78d8c6f23a83222cf3fd9fb48fb
eduardo rezende cruz <edu_rezende11@hotmail.com>
2020-12-29 23:11:39 -0300

    adicionando estilo e interatividade ao menu

---
+++ assets/scripts/index.js
@@ -0,0 +1,13 @@
+const navToggle = document.querySelector('.nav-toggle')
+const navLinks = document.querySelectorAll('.nav-link')
+
+navToggle.addEventListener('click',() => {
+    document.body.classList.toggle('nav-open')
+})
+/*add a class in the menu toggle  */
+
+navLinks.forEach(links => {
+    links.addEventListener('click',() => {
+        document.body.classList.remove('nav-open')
+    })
+})
